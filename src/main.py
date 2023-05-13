6bdcb157b60bf4d42c6108e2bbbda1de2f4bc08c
Aleksej Kucerenko <kuc.aleksej@gmail.com>
2023-05-13 09:08:07 +0200

    v0.2 - made initial version of model training

--- src/main.py
+++ src/main.py
@@ -1,16 +1,25 @@
 import glob
+import sys
+
 import torch
 import pandas as pd
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
 import torchvision
 import torchvision.transforms as transforms
 import nibabel as nib
 from pathlib import Path
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
+from model import BasicModel
 
 ROOT_PATH = Path('/Users/aleksej/IdeaProjects/master-thesis-kucerenko/UKE_SPECT')
 IMG_2D_RIGID_PATH = ROOT_PATH.joinpath('rigid', '2d')
 LABELS_PATH = ROOT_PATH.joinpath('scans_20230424.xlsx')
+
+RANDOM_SEED = 1234
 
 
 def get_data():
@@ -39,14 +48,63 @@ def get_data():
     # use only rater 'R1' for now
     labels = labels['R1'].to_numpy()
     labels = torch.from_numpy(labels)
+    #
+    labels = labels.view(-1, 1)
 
     return features, labels
 
 
 def main():
     features, labels = get_data()
-    print(features.size())
-    print(labels.size())
+    features = features.float()
+    labels = labels.float()
+
+    #   train-test split
+    X_train, X_test, y_train, y_test = train_test_split(features, labels,
+                                                        test_size=0.3,
+                                                        random_state=RANDOM_SEED,
+                                                        shuffle=True,)
+
+    # test-validation split
+    X_test, X_validation, y_test, y_validation = train_test_split(X_test, y_test,
+                                                                  test_size=0.5,
+                                                                  random_state=RANDOM_SEED,
+                                                                  shuffle=True,)
+
+    dataset = TensorDataset(X_train, y_train)
+
+    batch_size = 32
+
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    print(dataloader)
+
+    model = BasicModel()
+    loss_fn = nn.BCELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    num_epochs = 20
+
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        for batch_features, batch_labels in dataloader:
+            # Set model to train mode
+            model.train()
+
+            # Forward pass
+            outputs = model(batch_features)
+            loss = loss_fn(outputs, batch_labels)
+
+            # Backward pass and optimization
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # Accumulate the loss for printing
+            running_loss += loss.item()
+
+        # Average loss for the epoch
+        epoch_loss = running_loss / len(dataloader)
+        print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {epoch_loss}")
 
 
 if __name__ == '__main__':
---
+++ src/model.py
@@ -0,0 +1,42 @@
+import torch.nn as nn
+
+
+class BasicModel(nn.Module):
+    def __init__(self):
+        super(BasicModel, self).__init__()
+
+        channels_input, heigth, width = 1, 91, 109
+
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(128 * (91 // 8) * (109 // 8), 512)  # Adjusted dimensions
+        self.fc2 = nn.Linear(512, 64)
+        self.fc3 = nn.Linear(64, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.conv3(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.flatten(x)
+
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+        x = self.fc3(x)
+        x = self.sigmoid(x)
+
+        return x
