19ecbe947efcd60062337f21e1db432a90d660c8
Aleksej Kucerenko <kuc.aleksej@gmail.com>
2023-05-18 15:14:21 +0200

    v0.4 - made base_config.yaml and multiple custom yamls

--- src/main.py
+++ src/main.py
@@ -5,22 +5,26 @@ import yaml
 #   Path relative to current file
 configs_dirpath = 'configs'
 
+#   Base config (can be overriden by custom config!)
+
+base_config_path = f'{configs_dirpath}/base_config.yaml'
+
 #   Run experiment for rigid case and 2D images:
 
-config_rigid_2d_path = f'{configs_dirpath}/config_rigid_2d.yaml'
-
-with open(config_rigid_2d_path, 'r') as f:
-    config_rigid_2d = yaml.safe_load(f)
+with open(base_config_path, 'r') as f_base, open(f'{configs_dirpath}/config_rigid_2d.yaml', 'r') as f_custom:
+    config_rigid_2d_config = yaml.safe_load(f_base)
+    #   Update with custom changes
+    config_rigid_2d_config.update(yaml.safe_load(f_custom))
 
 run_experiment(experiment_name='rigid_2d',
-               config=config_rigid_2d)
+               config=config_rigid_2d_config)
 
 #   Run experiment for affine case and 2D images:
 
-config_affine_2d_path = f'{configs_dirpath}/config_affine_2d.yaml'
-
-with open(config_affine_2d_path, 'r') as f:
-    config_affine_2d = yaml.safe_load(f)
+with open(base_config_path, 'r') as f_base, open(f'{configs_dirpath}/config_affine_2d.yaml', 'r') as f_custom:
+    config_affine_2d_config = yaml.safe_load(f_base)
+    #   Update with custom changes
+    config_affine_2d_config.update(yaml.safe_load(f_custom))
 
 run_experiment(experiment_name='affine_2d',
-               config=config_affine_2d)
+               config=config_affine_2d_config)
