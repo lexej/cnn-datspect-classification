04a318554e4afde23dfa86b22d25b4cf91833bfe
Aleksej Kucerenko <kuc.aleksej@gmail.com>
2023-06-06 17:04:10 +0200

    v0.8 - added seed for gpu torch.mps

--- src/experiment.py
+++ src/experiment.py
@@ -6,12 +6,18 @@ import sys
 from typing import List
 from tqdm import tqdm
 
+import nibabel as nib
+
+import pandas as pd
+
 import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import Subset, DataLoader
+from torch.utils.data import Dataset, Subset, DataLoader
+import torchvision.transforms as transforms
+from torchvision.transforms import InterpolationMode
 
 import sklearn
 from sklearn.model_selection import train_test_split
@@ -20,21 +26,117 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 
 from src.model.custom_model_2d import CustomModel2d
 from src.model.resnet_2d import ResNet2d
-from src.preprocessing.data import SpectDataset
 
 RANDOM_SEED = 1327
 
-# Set all seeds
+dtype = torch.float
 
 torch.manual_seed(RANDOM_SEED)
-torch.cuda.manual_seed(RANDOM_SEED)
-torch.cuda.manual_seed_all(RANDOM_SEED)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
+
+#   Set device based on availability
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+
+    # Set seeds
+    torch.cuda.manual_seed(RANDOM_SEED)
+    torch.cuda.manual_seed_all(RANDOM_SEED)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+
+    #   Set seeds
+    torch.mps.manual_seed(RANDOM_SEED)
+    torch.backends.mps.deterministic = True
+    torch.backends.mps.benchmark = False
+else:
+    #   TODO: Throw error
+    pass
+
+
 generator = torch.Generator()
 generator.manual_seed(RANDOM_SEED)
 
 sklearn.utils.check_random_state(RANDOM_SEED)
+
+
+class SpectDataset(Dataset):
+    def __init__(self, features_dirpath, labels_filepath, target_input_height,
+                 target_input_width, interpolation_method):
+        self.features_dirpath = features_dirpath
+        self.labels_filepath = labels_filepath
+
+        self.target_input_height = target_input_height
+        self.target_input_width = target_input_width
+        self.interpolation_method = interpolation_method
+
+        self.labels = pd.read_excel(labels_filepath)
+
+        #   Sort filenames by name
+        self.image_filenames = sorted(os.listdir(features_dirpath))
+
+    def __len__(self):
+        return len(self.image_filenames)
+
+    def __getitem__(self, idx):
+
+        #   ---------------------------------------------------------------------------
+        #   Get image
+
+        image_filename = self.image_filenames[idx]
+
+        image_path = os.path.join(self.features_dirpath, image_filename)
+
+        data_nifti = nib.load(image_path)
+
+        img = data_nifti.get_fdata()
+
+        img = self.apply_transforms(img)
+
+        #   ---------------------------------------------------------------------------
+        #   Get label
+
+        label_row = self.labels.loc[self.labels['ID'].astype(int) == idx+1]
+
+        #   Convert pandas DataFrame row to pandas Series of labels of interest
+        label_row = label_row[['R1', 'R2', 'R3']].reset_index(drop=True).squeeze()
+
+        #   Majority value of the pandas Series
+
+        label = int(label_row.mode()[0])
+
+        #   To torch tensor (with additional dimension)
+
+        label = torch.tensor(label, dtype=dtype, device=device).unsqueeze(0)
+
+        return img, label
+
+    def apply_transforms(self, img: np.ndarray) -> torch.Tensor:
+
+        #   1. Crop image of size (91, 109) to region of interest (91, 91)
+        #       policy: select all rows and 10th to 100th column
+
+        transformed_img = img[:, 9:100]
+
+        #   2. Convert to torch tensor
+        #   3. Resize images from (91, 91) to target size (input_height, input_width)
+        #           using certain interpolation method
+        #       - Attention: Interpolation method has impact on model performance !
+
+        img_transforms = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Resize(size=(self.target_input_height, self.target_input_width),
+                              interpolation=getattr(InterpolationMode, self.interpolation_method.upper()),
+                              antialias=True)
+        ])
+
+        transformed_img = img_transforms(transformed_img)
+
+        #   4. Convert to float type and move to device
+
+        transformed_img = transformed_img.to(device=device, dtype=dtype)
+
+        return transformed_img
 
 
 def run_experiment(config: dict):
@@ -312,6 +414,9 @@ def run_experiment(config: dict):
     else:
         #   TODO
         pass
+
+    #   Move model to device and set data type
+    model = model.to(device=device, dtype=dtype)
 
     loss_fn = nn.BCELoss()
     optimizer = optim.Adam(params=model.parameters(), lr=lr)
