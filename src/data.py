aece54b5957e3641ce069598901af2bb70996949
Aleksej Kucerenko <kuc.aleksej@gmail.com>
2023-06-28 18:18:29 +0200

    v1.3.1 - split of get_dataloader function from experiment.py

--- src/data.py
+++ src/data.py
@@ -1,4 +1,8 @@
-from common import *
+from common import os, np, pd, nib
+from common import torch, transforms, Dataset, Subset, DataLoader, InterpolationMode
+from common import RANDOM_SEED, generator
+from common import device
+from common import train_test_split
 
 
 class SpectDataset(Dataset):
@@ -101,3 +105,109 @@ class LabelFunctionWrapper(Dataset):
     def __len__(self):
         return len(self.original_dataset)
 
+
+def choose_label_from_available_labels(label: dict, label_selection_strategy: str) -> torch.Tensor:
+
+    intra_rater_consensus_labels = {key: label[key] for key in ['R1', 'R2', 'R3']}
+
+    available_labels = list(intra_rater_consensus_labels.values())
+
+    if label_selection_strategy == 'random':
+        chosen_label = np.random.choice(available_labels)
+    elif label_selection_strategy == 'majority':
+        chosen_label = np.bincount(available_labels).argmax()
+    else:
+        raise Exception("Invalid label_selection_strategy passed.")
+
+    #   Wrap as torch tensor
+    chosen_label = torch.tensor(data=[int(chosen_label)],
+                                dtype=torch.float32,
+                                device=device)
+
+    return chosen_label
+
+
+def get_dataloaders(images_dirpath,
+                    labels_filepath,
+                    batch_size,
+                    test_to_train_split_size_percent,
+                    valid_to_train_split_size_percent,
+                    target_input_height,
+                    target_input_width,
+                    label_selection_strategy_train,
+                    label_selection_strategy_valid,
+                    interpolation_method: str):
+
+    # -----------------------------------------------------------------------------------------------------------
+    #   Create dataset
+
+    spect_dataset = SpectDataset(features_dirpath=images_dirpath,
+                                 labels_filepath=labels_filepath,
+                                 target_input_height=target_input_height,
+                                 target_input_width=target_input_width,
+                                 interpolation_method=interpolation_method)
+
+    # -----------------------------------------------------------------------------------------------------------
+
+    #   Splitting the data
+
+    #   1. train-test split  (no stratify used)
+
+    train_indices, test_indices = train_test_split(range(len(spect_dataset)),
+                                                   test_size=test_to_train_split_size_percent,
+                                                   random_state=RANDOM_SEED,
+                                                   shuffle=True)
+
+    train_subset = Subset(dataset=spect_dataset, indices=train_indices)
+    test_subset = Subset(dataset=spect_dataset, indices=test_indices)
+
+    #  2. test-validation split  (no stratify used)
+
+    train_indices, val_indices = train_test_split(range(len(train_subset)),
+                                                  test_size=valid_to_train_split_size_percent,
+                                                  random_state=RANDOM_SEED,
+                                                  shuffle=True)
+
+    #   First create validation subset, then reassign train subset !
+
+    val_subset = Subset(dataset=train_subset, indices=val_indices)
+
+    train_subset = Subset(dataset=train_subset, indices=train_indices)
+
+    #   train and valid Subsets are wrapped into a Dataset
+    #   which applies a mapping function to the labels using a certain label selection strategy
+
+    train_subset = LabelFunctionWrapper(original_dataset=train_subset,
+                                        label_function=choose_label_from_available_labels,
+                                        label_selection_strategy=label_selection_strategy_train)
+
+    val_subset = LabelFunctionWrapper(original_dataset=val_subset,
+                                      label_function=choose_label_from_available_labels,
+                                      label_selection_strategy=label_selection_strategy_valid)
+
+    # -----------------------------------------------------------------------------------------------------------
+
+    #   TODO : Anpassen normalize function ; auch: normalize ja/nein?
+
+    #  Normalize all splits
+
+    #   X_train, X_test, X_validation = normalize_data_splits(X_train, X_test, X_validation)
+
+    # -----------------------------------------------------------------------------------------------------------
+
+    #   Create data loaders for the data subsets
+
+    train_dataloader = DataLoader(dataset=train_subset,
+                                  batch_size=batch_size,
+                                  shuffle=True,  # True in order to shuffle train data before each new epoch
+                                  generator=generator)
+
+    valid_dataloader = DataLoader(dataset=val_subset,
+                                  batch_size=batch_size,
+                                  shuffle=False)  # Already randomly shuffled
+
+    test_dataloader = DataLoader(dataset=test_subset,
+                                 batch_size=batch_size,
+                                 shuffle=False)  # Already randomly shuffled
+
+    return train_dataloader, valid_dataloader, test_dataloader
